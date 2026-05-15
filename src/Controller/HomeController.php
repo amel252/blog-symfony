083a11c16be657f103a9442fe02d96d35d6b02b1
Amel252 <amelghanem25@gmail.com>
2026-05-16 01:58:40 +0200

    Mise a jour des fichiers + ajout template Story

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
+    //  route
+    // #[Route('/home', name: 'app_home', methods: ['GET'])]
+    // public function home(): Response{
+    //     return new Response('page home');
+    // }
 }
